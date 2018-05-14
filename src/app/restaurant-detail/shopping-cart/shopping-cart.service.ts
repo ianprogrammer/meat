2441373a24be2878cc3180dd722b9f507fd38af2
Ian Oliveira <ianprogrammer@gmail.com>
2018-05-14 10:44:26 -0300

    a

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -19,11 +19,21 @@ export class ShoppingCartService{
     addItem(item:MenuItem){
      let foundItem = this.items.find((mItem)=>mItem.menuItem.id === item.id)
     if(foundItem){
-        foundItem.quantity = foundItem.quantity + 1
+        this.increaseQty(foundItem)
     }else{
         this.items.push(new CartItem(item))
     }
 
+    }
+
+    decreaseQty(item:CartItem){
+        item.quantity = item.quantity - 1
+        if(item.quantity===0){
+            this.remove(item)
+        }
+    }
+    increaseQty(item:CartItem){
+        item.quantity = item.quantity + 1
     }
 
     remove(item:CartItem){
